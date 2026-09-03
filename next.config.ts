2fd1965e57bfaa333c0208719af2741f3564fe0f
chaeyoung20 <ceayoung15@gmail.com>
2026-09-03 16:54:19 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: { unoptimized: true },
-  basePath: process.env.GITHUB_ACTIONS ? '/portfolio2026' : '',
 };
 
 export default nextConfig;
